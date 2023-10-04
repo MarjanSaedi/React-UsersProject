26a3b77113a6f506d1ce85b63af3d432e757c99c
MarjanSaedi <marjan.saedi@gmail.com>
2023-10-03 23:17:58 -0600

    add validate method for login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Input from './input';
+import * as yup from 'yup'
+import axios from 'axios';
 
 class Login extends Component {
     state ={
         account : {
             email : '',
             password: ''
-        }
+        },
+        errors : []
     }
+
+    schema = yup.object().shape(
+        {
+            email: yup.string().email('Wrong format').required(),
+            password: yup.string().min(4, "your password should be mor than 4 character")
+        }
+    )
 
     hanleSubmit = async(e) =>{
         e.preventDefault();
+        const result = await this.validate();
+        console.log(result);
+        if(result){
+            const response = await axios.post('https://reqres.in/api/login', result);
+            console.log(response);
+        }
     }
 
     handleChange = async({currentTarget : input}) =>{
@@ -18,17 +34,42 @@ class Login extends Component {
         account[input.name] = input.value;
         this.setState({account});
     }
+
+    validate = async() => {
+        try{
+            const result = await this.schema.validate(this.state.account, {abortEarly : false});
+            return (result);
+        }
+        catch(error){
+            console.log(error.errors);
+            this.setState({errors : error.errors})
+        }
+    }
     
     render() { 
         const {email, password} = this.state.account;
         return (
-            <form onSubmit={this.hanleSubmit}>                
-                <Input name="email"  value={email} label="Email" onChange={this.handleChange}/>
-                <Input name="password"  value={password} label="Password" onChange={this.handleChange}/>
-                <div>
-                <button className='btn btn-primary'>Login</button>
-                </div>
-            </form>
+            <>
+                {
+                    this.state.errors.length !== 0 && (
+                        <div className='alert alert-danger'>
+                            <ul>
+                                {this.state.errors.map((e,i) => 
+                                <li key={i}>{e}</li>
+                                )}
+                            </ul>
+                        </div>
+                    )
+                }   
+                <form onSubmit={this.hanleSubmit}>                
+                    <Input name="email"  value={email} label="Email" onChange={this.handleChange}/>
+                    <Input name="password"  value={password} label="Password" onChange={this.handleChange}/>
+                    <div>
+                    <button className='btn btn-primary'>Login</button>
+                    </div>
+                </form>
+            </>
+            
         );
     }
 }
